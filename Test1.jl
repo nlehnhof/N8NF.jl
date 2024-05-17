36025937d1b2d24351c5ef6b33c142c1fe33393b
nlehnhof <n.lehnhof01@gmail.com>
2024-05-16 22:18:02 -0600

    did it?

---
+++ Test1.jl
@@ -0,0 +1,12 @@
+using PyCall
+using Conda
+
+Conda.add("aerosandbox")
+
+# Using packages
+
+# pyimport_conda("aerosandbox", PKG)`
+# asb = pyimport("aerosandbox")
+# np = pyimport("numpy")
+# import aerosandbox as asb
+# import aerosandbox.numpy as np
